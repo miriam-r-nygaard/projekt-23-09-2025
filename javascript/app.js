e464ba702c09d750adf2ed419ce927418e06556f
miriam-r-nygaard <eaa25miny@students.eaaa.dk>
2025-09-24 13:28:33 +0200

    draft three

--- javascript/app.js
+++ javascript/app.js
@@ -3,6 +3,12 @@ document.body.innerHTML += "Welcome";
 console.log("Hello Dat.js");
 function sayHello() {
   document.querySelector("h3").textContent =
-    "Hej " + document.querySelector("input").value;
+    "Hej " + document.querySelector("input#navn").value;
 }
 document.querySelector("button").addEventListener("click", sayHello);
+
+//function sayHello() {
+//document.querySelector("h3").textContent =
+//" " + document.querySelector("input#alder").value;
+//}
+//document.querySelector("button").addEventListener("click", sayHello);
